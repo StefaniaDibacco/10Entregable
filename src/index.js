27c751117ad298a4ad1a41955ce97d68621c5208
Stefania <stefania.dibacco@gmail.com>
2021-08-10 21:37:41 -0300

    caca

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import express from 'express';
 import path from 'path';
 import routerProductos from './routes/productos.js';
+import handlebars from 'express-handlebars';
 
 
 /** INICIALIZACION API con EXPRESS */
 const app = express();
 const puerto = 8080;
 
-const publicPath = path.resolve(__dirname, '../public');
-app.use(express.static(publicPath));
+const publicFolderPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicFolderPath));
+
+const layoutFolderPath = path.resolve(__dirname, '../views/layouts');
+const defaultLayerPth = path.resolve(__dirname, '../views/layouts/index.hbs');
+const partialFolderPath = path.resolve(__dirname, '../views/partial');
+app.set('view engine', 'hbs');
+
+app.engine(
+  'hbs',
+  handlebars({
+    layoutsDir: layoutFolderPath,
+    partialsDir: partialFolderPath,
+    defaultLayout: defaultLayerPth,
+    extname: 'hbs',
+  })
+);
+
+
+app.get('/', (req, res) => {
+  const listaDinamica ={
+  nombre: 'calculadora',
+  precio: 200,
+  url: 'url',
+  }
+  res.render('main', listaDinamica);
+});
+
+
+//const publicPath = path.resolve(__dirname, '../public');
+//app.use(express.static(publicPath));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
